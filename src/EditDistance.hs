297380e24dfcedb81fd6cc7b10e7465e15dedea4
cutsea110 <cutsea110@gmail.com>
2021-08-30 07:39:04 +0900

    edit distance for more detail info

--- src/EditDistance.hs
+++ src/EditDistance.hs
@@ -1,10 +1,20 @@
 module EditDistance where
 
 import Control.Monad.State       (State, get, put, evalState)
+import Data.List (transpose)
 import qualified Data.Map as Map (Map, lookup, insert, empty)
 
 type Table k v = Map.Map k v
 type Memo a b = State (Table a b) b
+
+data OpC = Ins Char | Del Char | Subst Char Char | Keep Char
+type IntOps = (Int, [OpC])
+
+instance Show OpC where
+  show (Ins c)      = ['-', 'v',  c ]
+  show (Del c)      = [ c,  '^', '-']
+  show (Subst c d ) = [ c,  '!',  d ]
+  show (Keep c)     = [ c,  ' ',  c ]
 
 memoise :: Ord a => (a -> Memo a b) -> a -> Memo a b
 memoise f x = do
@@ -19,17 +29,36 @@ memoise f x = do
 runM :: (a -> Memo a b) -> a -> b
 runM m v = evalState (m v) Map.empty
 
-edM :: (String, String) -> Memo (String, String) Int
-edM ([],       []      ) = return 0
-edM (xs@(_:_), []      ) = return (length xs)
-edM ([],       ys@(_:_)) = return (length ys)
+edM :: (String, String) -> Memo (String, String) IntOps
+edM ([],       []      ) = return (0, [])
+edM (xs@(_:_), []      ) = return (length xs, map Del xs)
+edM ([],       ys@(_:_)) = return (length ys, map Ins ys)
 edM xys@(_:_,   _:_    ) = memoise edM' xys
   where
     edM' (xxs@(x:xs), yys@(y:ys)) = do
-      a <- edM (xs,  ys)
-      b <- edM (xxs, ys)
-      c <- edM (xs,  yys)
-      return (minimum [ (if x == y then 0 else 1) + a, 1 + b, 1 + c ])
+      (a, ops_a) <- edM (xs,  ys)
+      (b, ops_b) <- edM (xxs, ys)
+      (c, ops_c) <- edM (xs,  yys)
+      return (min3
+               (if x == y
+                 then (a, Keep x:ops_a)
+                 else (1+a, Subst x y:ops_a))
+               (1+b, Ins y:ops_b)
+               (1+c, Del x:ops_c))
 
-edMemo :: (String, String) -> Int
+min3 :: Ord a => (a, b) -> (a, b) -> (a, b) -> (a, b)
+min3 a b c = if a `le` b then a `min2` c else b `min2` c
+  where
+    (x, _) `le` (y, _) = x <= y
+    min2 x y | x `le` y  = x
+             | otherwise = y
+
+edMemo :: (String, String) -> IntOps
 edMemo = runM edM
+
+edAll :: (String, String) -> IO ()
+edAll xys = do
+  putStrLn $ show d
+  mapM_ putStrLn $ transpose $ map show ops
+  where
+    (d, ops) = edMemo xys
