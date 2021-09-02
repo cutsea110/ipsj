49643a9c79552f6931840ab0b7ada22de8f96df8
cutsea110 <cutsea110@gmail.com>
2021-09-03 08:51:33 +0900

    smile functional programmer

--- src/CrowdOfFunctionalProgrammers.hs
+++ src/CrowdOfFunctionalProgrammers.hs
@@ -7,6 +7,8 @@ man = segmentsToPainter 20 20
       [[ (6, 10), (0, 10), (0, 12), (6, 12), (6, 14), (4, 16), (4, 18), (6, 20), (8, 20), (10, 18)
        , (10, 16), (8, 14), (8, 12), (10, 12), (10, 14), (12, 14), (12, 10), (8, 10), (8, 8), (10, 0)
        , (8, 0), (7, 4), (6, 0), (4, 0), (6, 8), (6, 10)]
+      , [(5, 18), (5, 17)], [(9, 18), (9, 17)] -- eyes
+      , [(7, 15), (8, 16), (6, 16), (7, 15)]   -- mouth
       ]
 
 horiz :: Float -> Painter -> Painter
@@ -23,4 +25,4 @@ crowd xs p = foldl f blank $ zip3 (rs++[rest]) (0:accs) (ps++[blank])
         f q (r, s, p) = above r s p q
 
 main :: IO ()
-main = crowd [5..10] man unitSquare
+main = crowd [3, 5, 7, 11, 13] man unitSquare
