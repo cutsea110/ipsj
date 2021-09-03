1698e5351c57e6966ee139b7d28589c35d8c81c1
cutsea110 <cutsea110@gmail.com>
2021-09-03 09:00:54 +0900

    start to implement sudoku

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -25,3 +25,21 @@ sample = [ [8, 0, 0, 0, 3, 4, 0, 5, 0]
          , [2, 0, 0, 0, 0, 0, 1, 0, 0]
          , [0, 9, 0, 5, 6, 0, 0, 0, 7]
          ]
+
+type Position = (Int, Int) -- 列 x 行
+
+sudoku :: SudokuBoard -> [SudokuBoard]
+sudoku b
+  = case vacantPositions b of
+      [] -> [b]
+      ps -> case nextVacant b ps of
+              (p, xs) -> concatMap (sudoku . putCell b) [(p, x) | x <- xs]
+
+vacantPositions :: SudokuBoard -> [Position]
+vacantPositions = error "(vacantPositions) is not yet implemented"
+
+nextVacant :: SudokuBoard -> [Position] -> (Position, [Sudoku])
+nextVacant = error "(nextVacant) is not yet implemented"
+
+putCell :: SudokuBoard -> (Position, Sudoku) -> SudokuBoard
+putCell = error "(putCell) is not yet implemented"
