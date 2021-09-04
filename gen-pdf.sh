fb4fd22d4266840f9b5b690ae0a682ba349c3938
cutsea110 <cutsea110@gmail.com>
2021-09-05 08:24:41 +0900

    update

--- gen-pdf.sh
+++ gen-pdf.sh
@@ -2,15 +2,14 @@
 
 set -eu
 
+rm *.eps *.pdf
+
 runhaskell -isrc src/CrowdOfFunctionalProgrammers.hs > crowd-of-fp.eps
 runhaskell -isrc src/Fish.hs > fish.eps
 runhaskell -isrc src/Hilbert.hs > hilbert.eps
 runhaskell -isrc src/RhinecanthusRectangulus.hs > school-of-humuhumu.eps
 runhaskell -isrc src/Wave.hs > wave.eps
 
-epspdf crowd-of-fp.eps
-epspdf fish.eps
-epspdf hilbert.eps
-epspdf school-of-humuhumu.eps
-epspdf wave.eps
-
+for f in `ls *.eps`; do
+    epspdf $f
+done
