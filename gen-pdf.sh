b5601575abb06b0bf637c5b6a7246e563b91d393
cutsea110 <cutsea110@gmail.com>
2021-09-06 07:20:51 +0900

    update script

--- gen-pdf.sh
+++ gen-pdf.sh
@@ -2,14 +2,17 @@
 
 set -eu
 
-rm *.eps *.pdf
+OUT_DIR=eps
 
-runhaskell -isrc src/CrowdOfFunctionalProgrammers.hs > crowd-of-fp.eps
-runhaskell -isrc src/Fish.hs > fish.eps
-runhaskell -isrc src/Hilbert.hs > hilbert.eps
-runhaskell -isrc src/RhinecanthusRectangulus.hs > school-of-humuhumu.eps
-runhaskell -isrc src/Wave.hs > wave.eps
+rm -rf ${OUT_DIR}
+mkdir -p ${OUT_DIR}
 
-for f in `ls *.eps`; do
+runhaskell -isrc src/CrowdOfFunctionalProgrammers.hs > ${OUT_DIR}/crowd-of-fp.eps
+runhaskell -isrc src/Fish.hs > ${OUT_DIR}/fish.eps
+runhaskell -isrc src/Hilbert.hs > ${OUT_DIR}/hilbert.eps
+runhaskell -isrc src/RhinecanthusRectangulus.hs > ${OUT_DIR}/school-of-humuhumu.eps
+runhaskell -isrc src/Wave.hs > ${OUT_DIR}/wave.eps
+
+for f in `ls ${OUT_DIR}/*.eps`; do
     epspdf $f
 done
