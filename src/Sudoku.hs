e3b6dcaf6fbb28ba5fd8e40308a8e97b4e30a335
cutsea110 <cutsea110@gmail.com>
2021-09-06 09:11:23 +0900

    rewrite Sudoku by using Puzzle module

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -3,80 +3,55 @@ module Sudoku where
 
 import Data.List (nub, minimumBy, transpose, (\\))
 
+import Puzzle
+
+type Sudoku = Int
+
+instance Puzzle Sudoku where
+  puzzleElm     = [1..sudokuSize]
+  vacant        = 0
+  candidate b p = puzzleElm \\ nub (concatMap (\c -> c b p) [col, row, box])
+
 sudokuBase :: Int
 sudokuBase = 3
 
 sudokuSize :: Int
 sudokuSize = sudokuBase^2
 
-type Sudoku = Int
-sudokuElm :: [Sudoku]
-sudokuElm = [1..sudokuSize]
-vacant :: Sudoku
-vacant = 0
-
-type SudokuBoard = [[Sudoku]]
-
-sample :: SudokuBoard
-sample = [[8,0,0,0,3,4,0,5,0]
-         ,[0,0,2,0,0,0,0,0,1]
-         ,[0,1,0,9,0,0,0,0,0]
-         ,[0,0,8,0,0,9,0,0,6]
-         ,[5,0,0,0,1,0,0,0,8]
-         ,[6,0,0,4,0,0,7,0,0]
-         ,[0,0,0,0,0,1,0,7,0]
-         ,[2,0,0,0,0,0,1,0,0]
-         ,[0,9,0,5,6,0,0,0,2]
-         ]
-
-type Position = (Int, Int) -- 列 x 行
-
-sudoku :: SudokuBoard -> [SudokuBoard]
-sudoku b
-  = case vacantPositions b of
-      [] -> [b]
-      ps -> case nextVacant b ps of
-              (p, xs) ->
-                concatMap sudoku
-                $ map (putCell b)
-                $ [(p, x) | x <- xs]
-
-vacantPositions :: SudokuBoard -> [Position]
-vacantPositions b = map fst
-                    $ filter (\(p, x) -> vacant == x)
-                    $ concatMap (\(j, xs) -> zipWith (\i x -> ((i, j), x)) [0..] xs)
-                    $ zip [0..] b
-
-nextVacant :: SudokuBoard -> [Position] -> (Position, [Sudoku])
-nextVacant b ps = minimumBy cmp [(p, candidate b p) | p <- ps]
-  where (_, xs) `cmp` (_, ys) = length xs `compare` length ys
-
-candidate :: SudokuBoard -> Position -> [Sudoku]
-candidate b p = sudokuElm \\ nub (concat $ map (\c -> c b p) [col, row, box])
-
-col :: SudokuBoard -> Position -> [Sudoku]
+col :: Board Sudoku -> Position -> [Sudoku]
 col b (i, _) = transpose b !! i
-row :: SudokuBoard -> Position -> [Sudoku]
+row :: Board Sudoku -> Position -> [Sudoku]
 row b (_, j) = b !! j
-box :: SudokuBoard -> Position -> [Sudoku]
+box :: Board Sudoku -> Position -> [Sudoku]
 box b (i, j) = concat
                $ map (take sudokuBase) $ map (drop $ (i `div` sudokuBase) * sudokuBase)
                $ take sudokuBase $ drop ((j `div` sudokuBase) * sudokuBase) b
 
+sudoku :: Board Sudoku -> [Board Sudoku]
+sudoku = solve
 
-putCell :: SudokuBoard -> (Position, Sudoku) -> SudokuBoard
-putCell b ((i, j), x) = ls0 ++ (xs0 ++ x:xs1):ls1
-  where (ls0, l:ls1) = splitAt j b
-        (xs0, _:xs1) = splitAt i l
-
-showSudokuBoard :: SudokuBoard -> String
+showSudokuBoard :: Board Sudoku -> String
 showSudokuBoard b = unlines $ map (unwords . map show) b
 
-readSudokuBoard :: String -> SudokuBoard
+readSudokuBoard :: String -> Board Sudoku
 readSudokuBoard s = map (map read . words) $ lines s
 
-instance {-# OVERLAPPING #-} Show SudokuBoard where
+instance {-# OVERLAPPING #-} Show (Board Sudoku) where
   show = showSudokuBoard
 
-instance {-# OVERLAPPING #-} Read SudokuBoard where
+instance {-# OVERLAPPING #-} Read (Board Sudoku) where
   readsPrec _ str = [(readSudokuBoard str, "")]
+
+sample :: Board Sudoku
+sample = read sampleData
+sampleData :: String
+sampleData = unlines ["8 0 0 0 3 4 0 5 0"
+                     ,"0 0 2 0 0 0 0 0 1"
+                     ,"0 1 0 9 0 0 0 0 0"
+                     ,"0 0 8 0 0 9 0 0 6"
+                     ,"5 0 0 0 1 0 0 0 8"
+                     ,"6 0 0 4 0 0 7 0 0"
+                     ,"0 0 0 0 0 1 0 7 0"
+                     ,"2 0 0 0 0 0 1 0 0"
+                     ,"0 9 0 5 6 0 0 0 2"
+                     ]
